bee86449ed1689da46e85c050bae8d6602ddf673
Ankit Kumar Pandit <panditankit1995@gmail.com>
2024-04-10 02:54:34 +0530

    Folder structure update

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -2,5 +2,24 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  server: {
+    port: 3001,
+    host: true,
+  },
+  clearScreen: false,
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      onLog(level, log, handler) {
+        const cause = log.cause as any;
+        if (
+          cause &&
+          cause?.message === `Can't resolve original location of error.`
+        ) {
+          return;
+        }
+        handler(level, log);
+      },
+    },
+  },
 });
